c0399b1d1e8a11d77cf0686a4d7ecd308edcce7f
Pratham Arora <arorapratham6@gmail.com>
2018-08-28 20:12:14 +0530

    Updated token route

--- server/models/user.js
+++ server/models/user.js
@@ -108,7 +108,7 @@ UserSchema.methods.toJSON = function() {
     var user = this;
     var userObject = user.toObject();
 
-    return _.pick(userObject , ['_id' , 'emailID' , 'phoneNumber' , 'GSTNumber' , 'name' , 'transactions' , 'clients']);
+    return _.pick(userObject , ['_id' , 'emailID' , 'phoneNumber' , 'GSTNumber' , 'name' , 'transactions' , 'clients', 'tokens']);
 };
 
 
